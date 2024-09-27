8d5f28547677b20414d7f5b010f43c89f67f85aa
Chantell Osejo <6220053+chantellosejo@users.noreply.github.com>
2024-09-28 01:00:20 +0200

    Updated Kitchen Quest Speed Shopping Script to support SDB searches

--- Kitchen Quest Helper.user.js
+++ Kitchen Quest Helper.user.js
@@ -1,23 +1,42 @@
 // ==UserScript==
-// @name         Kitchen Quest Shop Wizard Helper
+// @name         Neopets Kitchen Quest Speed Shopping
 // @namespace    neopets.com
-// @version      1.1
-// @description  Adds a button for each requested ingredient in a Kitchen Quest for fast Shop Wizard searches.
+// @version      1.3
+// @description  Adds a button for each requested ingredient in a Kitchen Quest for fast Shop Wizard & Safety Deposit Box searches. Stores searched prices (SSW only)
+//               and displays the total cost of completing the quest.
 // @author       darknstormy
-// @match        http*://www.neopets.com/island/kitchen.phtml
-// @grant        none
 // @license      MIT
+// @match        http*://www.neopets.com/island/kitchen.phtml
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 function openShopWizardPage(itemName) {
     window.open(`https://www.neopets.com/shops/wizard.phtml?string=${itemName.replaceAll(" ","+")}`, '_blank')
 }
 
-function searchUsingSuperShopWizard(itemName) {
+function searchUsingSuperShopWizard(itemName, priceElement) {
     openSSW()
     $('.ssw-search__2020 #searchstr').val(itemName)
     $('#ssw-button-search .button-search-white__2020').click()
 
+     waitForElement(".ssw-results-grid").then((searchResults) => {
+        let lowestPrice = searchResults.find(".ssw-results-grid-price:first").text()
+        priceElement.text('Lowest Price: ' + lowestPrice)
+        let priceList = GM_getValue("priceList", {})
+        priceList[itemName] = Number(lowestPrice.replace(/\D/g, ''))
+        GM_setValue("priceList", priceList)
+        calculateTotal()
+    })
+}
+
+function calculateTotal() {
+    let priceList = GM_getValue("priceList", {})
+    let searchedPrices = $(".ingredient-grid").children('div').toArray().map((elem) => priceList[$(elem).find('p:first').text()]).filter((price) => price);
+    if (searchedPrices && searchedPrices.length > 0) {
+        let total = searchedPrices.reduce((accumulator, current) => accumulator + current)
+        $('#total_cost').text(total)
+    }
 }
 
 function openSSW() {
@@ -29,13 +48,13 @@ function openSSW() {
 function waitForElement(selector) {
     return new Promise(resolve => {
         if ($(selector).is(":visible")) {
-            return resolve($(selector));
+            return resolve($(selector))
         }
 
         const observer = new MutationObserver(mutations => {
             if ($(selector).is(":visible")) {
-                observer.disconnect();
-                resolve($(selector));
+                observer.disconnect()
+                resolve($(selector))
             }
         })
 
@@ -49,6 +68,8 @@ function waitForElement(selector) {
 
 $(document).ready(function() {
     waitForElement('.ingredient-grid').then(it => {
+        it.parent().prepend('<div style="display: flex; width: 100%; align-content: center;"><p><b>Total Cost for Searched Ingredients: <span id="total_cost"></span> NP</b></p></div>')
+
         it.children('div').each(function() {
             $(this).css("border", "4px solid lightgray")
             $(this).css("border-radius", "10px")
@@ -59,13 +80,23 @@ $(document).ready(function() {
 
             let name = nameElement.text()
 
-            $(this).append('<div style="display: flex; justify-content: space-between;  align-items: center; width: auto"><button id="sw" class="button-default__2020 button-yellow__2020" style="display: inline; width: 50%; margin: 8px;">SW</button><button id="ssw" class="button-default__2020 button-yellow__2020" style="display: inline; width: 50%; margin: 8px;">SSW</button></div>')
+            $(this).append('<div style="display: flex; flex-flow: row wrap; justify-content: space-between; align-items: center; width: auto"><button id="sw" class="button-default__2020 button-yellow__2020" style="display: inline; width: 40%; margin: 8px;">SW</button><button id="ssw" class="button-default__2020 button-yellow__2020" style="display: inline; width: 40%; margin: 8px;">SSW</button><button id="sdb" class="button-default__2020 button-yellow__2020" style="display: inline; width: 40%; margin: 8px;">SDB</button><p id="ssw-price"/></div>')
 
-            let swb = $(this).find('#sw')
-            swb.on('click', () => { openShopWizardPage(name) });
+            let price = GM_getValue("priceList", {})[name]
+            if (price) {
+                $(this).find("#ssw-price").text("Previously Searched Price: " + price + " NP")
+            }
 
-            let sswb = $(this).find('#ssw')
-            sswb.on('click', () => { searchUsingSuperShopWizard(name) });
+            let swButton = $(this).find('#sw')
+            swButton.on('click', () => { openShopWizardPage(name) })
+
+            let sswButton = $(this).find('#ssw')
+            sswButton.on('click', () => { searchUsingSuperShopWizard(name, $(this).find("#ssw-price")) })
+
+            let sdbButton = $(this).find('#sdb')
+            sdbButton.on('click', () => { window.open(`https://www.neopets.com/safetydeposit.phtml?obj_name=${encodeURIComponent(name)}&category=0`, "_blank") })
         })
+
+        calculateTotal()
     })
 })
